f8d334e02905018d9bcf3609c3f99c06c208468d
Emmanuel Nartey-Kwashie <emmanuelkwashie10@gmail.com>
2026-01-24 18:50:50 +0000

    Update site content/js

---
+++ index.js
@@ -0,0 +1,4 @@
+//literals are fixed values and variables are variable values
+age = "18";
+console.log(age);
+alert("this is an alert");
---
+++ syntax..js
@@ -0,0 +1,5 @@
+//unary operator
+console.log(typeof 4.5);
+console.log(-(10-2));
+console.log(typeof "x");
+//boolean values
