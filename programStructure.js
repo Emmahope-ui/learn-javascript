3afd49df2cfda146c047d0854c551cbd75b9a190
Emmanuel Nartey-Kwashie <emmanuelkwashie10@gmail.com>
2026-01-24 20:03:13 +0000

    Update site content/js

---
+++ programStructure.js
@@ -0,0 +1,19 @@
+//A fragment of code that produces a value is called an expression.
+//binding
+let emmanuelsDebt = 200;
+emmanuelsDebt = emmanuelsDebt - 50;
+console.log(emmanuelsDebt+"$");
+
+let one = 1, two = 2;
+console.log(one + two);
+
+var name = "Emmanuel";
+const greeting = "Hello";
+console.log(greeting + " " + name);
+
+//functions
+// prompt("Enter Password");
+
+//return values
+console.log(Math.max(2,4));
+console.log(Math.min(4,5) + 19)
--- syntax..js
+++ syntax..js
@@ -2,4 +2,22 @@
 console.log(typeof 4.5);
 console.log(-(10-2));
 console.log(typeof "x");
+
 //boolean values
+console.log(3 > 2);
+console.log(2>3);
+console.log("Emmanuel" != "Kwashie");
+console.log("Nartey"== "Kwashie")
+
+//NaN is not a number thats the only value not equal to itself
+console.log(NaN == NaN);
+
+//logical operators
+console.log(true && false);
+console.log(true && true);
+console.log(true || false);
+console.log(false || false);
+
+//ternary operator
+console.log(true ? 1 : 2);
+console.log(false ? 1 : 2);
