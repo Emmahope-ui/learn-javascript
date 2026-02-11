acf5ebd844d68e7e998452f0ec1437725d4616e1
Emmanuel Nartey-Kwashie <emmanuelkwashie10@gmail.com>
2026-02-11 12:24:38 +0000

    update site content/js

--- functions.js
+++ functions.js
@@ -11,9 +11,28 @@ function message() {
 }
 message();
 
-//3.0
+//3.0 parameters
 function staffs(from,text){
     console.log(from+":"+text);
 }
 staffs("Fred","hello");
-staffs("Fred","whats up")
+staffs("Fred","whats up");
+
+//4.0
+function ShowMessage(From,Text){
+    From = "*" + From + "*";
+    console.log(From + ':' + Text);
+}
+let From = "Samson";
+ShowMessage(From,"Hello");
+console.log(From);
+/*When a value is passed as a function parameter, it’s also called an argument.
+
+In other words, to put these terms straight:
+
+    A parameter is the variable listed inside the parentheses in the function declaration (it’s a declaration time term).
+    An argument is the value that is passed to the function when it is called (it’s a call time term).
+
+We declare functions listing their parameters, then call them passing arguments.
+
+In the example above, one might say: “the function showMessage is declared with two parameters, then called with two arguments: from and "Hello"”.*/
